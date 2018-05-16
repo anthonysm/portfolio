6bf68b391d32dd1026af56d4c0b156880bc04d19
Mikael Miranda <anthony31929@hotmail.com>
2018-05-16 22:47:12 +0200

    fin du portfolio

--- src/Controller/BobLeController.php
+++ src/Controller/BobLeController.php
@@ -1,5 +1,4 @@
 <?php
-
 // src/Controller/BobLeController.php
 
 namespace App\Controller;
@@ -36,9 +35,58 @@ class BobLeController extends Controller
   /**
   * @Route("/save_project", name="save_project")
   */
-  public function project()
+  public function saveProject()
   {
     return $this->render('save_projet.html.twig');
+  }
+  /**
+  * @Route("/project1", name="project1")
+  */
+  public function project1()
+  {
+    return $this->render('projet1.html.twig');
+  }
+  /**
+  * @Route("/project2", name="project2")
+  */
+  public function project2()
+  {
+    return $this->render('projet2.html.twig');
+  }
+  /**
+  * @Route("/project3", name="project3")
+  */
+  public function project3()
+  {
+    return $this->render('projet3.html.twig');
+  }
+  /**
+  * @Route("/project4", name="project4")
+  */
+  public function project4()
+  {
+    return $this->render('projet4.html.twig');
+  }
+  /**
+  * @Route("/project5", name="project5")
+  */
+  public function project5()
+  {
+    return $this->render('projet5.html.twig');
+  }
+  /**
+  * @Route("/project6", name="project6")
+  */
+  public function project6()
+  {
+    return $this->render('projet6.html.twig');
+  }
+  /**
+  * @Route("/project7", name="project7")
+  */
+  public function project7()
+  {
+    return $this->render('projet7.html.twig');
   }
   /**
   * @Route("/login", name="login")
